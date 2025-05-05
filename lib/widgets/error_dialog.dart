e3cf9d113313653599ecebdfd7c905e598a3f540
JoanLinares <joan.linares@gracia.lasalle.cat>
2025-05-05 22:31:03 +0200

    feat: dialog for errors widget

---
+++ lib/widgets/error_dialog.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void showError(BuildContext ctx, String msg) {
+  showDialog(
+    context: ctx,
+    builder: (_) => AlertDialog(
+      title: const Text('Error'),
+      content: Text(msg),
+      actions: [TextButton(onPressed: ()=>Navigator.pop(ctx), child: const Text('OK'))],
+    ),
+  );
+}
