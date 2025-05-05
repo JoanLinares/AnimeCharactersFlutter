b382c415821617cfd386ec9c26a21a992eabc3a4
JoanLinares <joan.linares@gracia.lasalle.cat>
2025-05-05 21:56:43 +0200

    refactor: Refactor of variables

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,45 +8,45 @@ import '../utils/constants.dart';
 
 class ApiService {
   Future<List<Character>> fetchCharacters() async {
-    final r = await http.get(Uri.parse(charsEP));
-    if (r.statusCode == 200) {
-      final data = json.decode(r.body) as List;
+    final characters = await http.get(Uri.parse(charsEP));
+    if (characters.statusCode == 200) {
+      final data = json.decode(characters.body) as List;
       return data.map((e) => Character.fromJson(e)).toList();
     }
     throw Exception('Error loading characters');
   }
 
   Future<List<Character>> fetchCharactersByCrew(int crewId) async {
-    final r = await http.get(Uri.parse('$crewById/$crewId'));
-    if (r.statusCode == 200) {
-      final data = json.decode(r.body) as List;
+    final crewsCharacters = await http.get(Uri.parse('$crewById/$crewId'));
+    if (crewsCharacters.statusCode == 200) {
+      final data = json.decode(crewsCharacters.body) as List;
       return data.map((e) => Character.fromJson(e)).toList();
     }
     throw Exception('Error loading crew #$crewId');
   }
 
   Future<Character> fetchCharacterById(int id) async {
-    final r = await http.get(Uri.parse('$charById/$id'));
-    if (r.statusCode == 200) {
-      return Character.fromJson(json.decode(r.body));
+    final characterID = await http.get(Uri.parse('$charById/$id'));
+    if (characterID.statusCode == 200) {
+      return Character.fromJson(json.decode(characterID.body));
     }
     throw Exception('Error loading character #$id');
   }
 
   Future<List<Crew>> fetchCrews() async {
-    final r = await http.get(Uri.parse(crewsEP));
-    if (r.statusCode == 200) {
-      final d = json.decode(r.body) as List;
-      return d.map((e) => Crew.fromJson(e)).toList();
+    final crews = await http.get(Uri.parse(crewsEP));
+    if (crews.statusCode == 200) {
+      final data = json.decode(crews.body) as List;
+      return data.map((e) => Crew.fromJson(e)).toList();
     }
     throw Exception('Error loading crews');
   }
 
   Future<List<Haki>> fetchHakisByCharacter(int id) async {
-    final r = await http.get(Uri.parse('$hakisByC/$id'));
-    if (r.statusCode == 200) {
-      final d = json.decode(r.body) as List;
-      return d.map((e) => Haki.fromJson(e)).toList();
+    final characterHaki = await http.get(Uri.parse('$hakisByC/$id'));
+    if (characterHaki.statusCode == 200) {
+      final data = json.decode(characterHaki.body) as List;
+      return data.map((e) => Haki.fromJson(e)).toList();
     }
     throw Exception('Error loading hakis');
   }
