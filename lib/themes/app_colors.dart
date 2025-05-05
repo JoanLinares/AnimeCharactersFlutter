f98ec5992969bc58f3347c4d8a624079be41b5c4
JoanLinares <joan.linares@gracia.lasalle.cat>
2025-05-05 20:53:05 +0200

    feat: App Colors

---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary    = Color(0xFFD32F2F);
+  static const accent     = Color(0xFFFFC107);
+  static const background = Color(0xFFFAF3D1);
+  static const textDark   = Color(0xFF3E2723);
+}
